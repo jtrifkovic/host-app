c35654cbd57a5f7bd56479e5c21379442a72cf9e
jtrifkovic <jowantrifkovic@gmail.com>
2023-03-30 15:32:25 +0200

    working on setting up the ui and api, plus adding /decks endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,28 @@
-import express from "express"
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import Deck from "./models/Deck";
 
-const app = express()
+const PORT = 5000
 
-app.get('/', (req, res) =>{
-    res.send("Hello World")
-})
+const app = express();
 
-app.listen(5000)
+app.use(express.json())
+
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title
+  })
+  await newDeck.save()
+  const createdDeck = await newDeck.save()
+  res.json(createdDeck)
+});
+
+
+mongoose.connect(
+    "mongodb+srv://host-app:p9lxX5FpTlVvKScB@cluster0.zifyomw.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log(`Listetning on port ${PORT}`)
+    app.listen(PORT);
+  });
+ 
