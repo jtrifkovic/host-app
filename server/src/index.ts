530cfd62c027c3e46f3cddc6595fb6e1107491b4
jtrifkovic <jowantrifkovic@gmail.com>
2023-03-31 15:06:39 +0200

    working on showing the decks on the landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,14 +4,25 @@ config()
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import Deck from "./models/Deck";
+import cors from "cors"
 
 
 const PORT = 5000
 
-
 const app = express();
 
+app.use(cors())
 app.use(express.json())
+
+app.get("/decks",  async (req: Request, res: Response) => {
+    //TODO: fetch all decks and send back to the user
+    // 1. how do we fetch the decks from mongo?
+    const decks = await Deck.find()
+    console.log(decks)
+    // 2. how do we send back the array to the ui?
+    res.json(decks)
+
+})
 
 app.post("/decks", async (req: Request, res: Response) => {
   const newDeck = new Deck({
